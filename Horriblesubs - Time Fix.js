c6ca0a5666607a2f4b76d98a43e95fa81ea89fc2
Mathias S <mathias.spanhove@hotmail.be>
2015-10-12 13:56:01 +0200

    Added daylight saving consideration

--- Horriblesubs - Time Fix.js
+++ Horriblesubs - Time Fix.js
@@ -1,37 +1,60 @@
 // ==UserScript==
-// @name         Horrible Subs Time Zone Fix
-// @version      1.0.2
+// @name         Horriblesubs Time Zone Fix
+// @version      1.1.0
 // @description  Release time for your timezone
 // @author       Cpt_Mathix
-// @match        *://horriblesubs.info/*
+// @match        http://horriblesubs.info/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.3.0/moment-timezone.min.js
 // @grant        none
+// @namespace https://greasyfork.org/users/16080
 // ==/UserScript==
 
+var currentTime = moment();
+var utcTime = moment.utc();
+moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
+var timeLA = moment.tz(currentTime, "America/Los_Angeles");
+// difference between UTC and LA
+var difference = utcTime.hours() - timeLA.hours();
+
+
 var el1 = document.getElementById('sidebar');
-init(el1);
+if (el1 != null)
+    init(el1);
 
 if(window.location.href.indexOf("schedule") > -1) {
     var el2 = document.getElementById('main');
-    init(el2);
+    if (el2 != null)
+        init(el2);
 }
 
 // go to https://www.timeanddate.com/clocks/free.html make your own clock and paste the src below
 var src = '';
 var schedule = document.getElementsByClassName("textwidget");
-if (src == '') {
-    schedule[2].childNodes[0].style.display = 'none';
-} else {
-    var timer = schedule[2].childNodes;
-    timer[0].src = src;
-    timer[0].width = '100';
+if (schedule.length != 0) {
+    for(var i = 0; i < schedule.length; i++) {
+        if (schedule[i].innerHTML.indexOf('timeanddate') > -1)
+            schedule = schedule[i];
+    }
+    var timer = schedule.childNodes[0];
+    if (src == '') {
+        schedule.parentNode.style.display = 'none';
+    } else {
+        timer.src = src;
+        timer.width = '120';
+        if (src.indexOf('cf100') > -1)
+            timer.height = '120';
+    }
 }
+
 
 function init(element) {     
     element.innerHTML = element.innerHTML.replace(/\d\d:\d\d/g, function(s) {    
         var mdy = s.split(':');
         var hour = Number(mdy[0]);
         var date = new Date();
-        var hour = hour + 7 - (date.getTimezoneOffset()/60);
+        var hour = hour + difference - (date.getTimezoneOffset()/60);
+        
         if (hour > 23) {
             var hour = hour - 24;
         }
